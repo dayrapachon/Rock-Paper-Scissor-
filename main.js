66c4ea98513bbec1c5c5438fe4b59e1f95dbb937
dayrapachon <123913850+dayrapachon@users.noreply.github.com>
2023-02-07 19:58:48 -0500

    Update main.js

--- main.js
+++ main.js
@@ -6,5 +6,63 @@ if (userInput === "rock" ||userInput === "paper"||userInput === "scissors" ) {
 } 
 
 function getComputerChoice () {
-    Math.floor(Math.random()* 3);
-} 
+    var computerNumber = Math.floor(Math.random() * 3);
+
+    if (computerNumber === 0){
+        return 'rock';
+    } else if (computerNumber === 1) {
+        return 'paper';
+    } else if (computerNumber === 2) {
+        return 'scissors'
+    }
+};
+
+function determineWinner (userChoice, computerChoice ) {
+    if (userChoice === computerChoice) {
+        return 'Game was a tie';
+    }
+}
+
+if (userChoice === 'rock') {
+    if (computerChoice === 'paper') {
+        return 'The computer won';
+    }
+}
+
+if (computerChoice === 'scissors'){
+    return 'the user won';
+}
+
+if (userChoice === 'paper') {
+    if (computerChoice === 'scissors') {
+        return 'The computer won';
+    }
+
+    if (computerChoice === 'rock') {
+        return 'the user won';
+    }
+}
+
+if (userChoice === 'scissors') {
+    if (computerChoice === 'rock') {
+        return 'the computer won';
+    }
+
+    if (computerChoice === 'paper') {
+        return 'the user won';
+    }
+};
+
+var playGame = function () {
+    var userChoice = getComputerChoice('rock');
+    var computerChoice = getComputerChoice();
+
+    console.log('user choise', userChoice);
+    console.log('computer choice'computerChoice);
+
+    var winner = determineWinner(userChoice, computerChoice);
+    console.log(winner);
+};
+
+playGame();
+
